c7d17c9de688ec2c67024a64cc0ebed3e8811d60
aschneider15 <aschneider15@outlook.com>
2022-03-31 20:19:31 -0400

    Creating the Map

---
+++ Assets/Scripts/CubeBehavior.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//small change
+
+public class CubeBehavior : MonoBehaviour
+{
+    public Vector3 jump = new Vector3(0.0f, 1.0f, 0.0f);
+    public Vector3 goForward = new Vector3(1.0f, 0.0f, 0.0f);
+    public Vector3 goBack = new Vector3(-1.0f, 0.0f, 0.0f);
+    public Vector3 goLeft = new Vector3(0.0f, 0.0f, -1.0f);
+    public Vector3 goRight = new Vector3(0.0f, 0.0f, 1.0f);
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        jump = new Vector3(0.0f, 1.0f, 0.0f);
+        goRight = new Vector3(1.0f, 0.0f, 0.0f);
+        goLeft = new Vector3(-1.0f, 0.0f, 0.0f);
+        goForward = new Vector3(0.0f, 0.0f, 1.0f);
+        goBack = new Vector3(0.0f, 0.0f, -1.0f);
+        speed = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody rb = this.gameObject.GetComponent<Rigidbody>();
+        
+        if(Input.GetKeyDown("space"))
+        {
+            rb.velocity = jump * speed;
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.velocity = goForward * speed;
+        }
+
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            rb.velocity = goBack * speed;
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rb.velocity = goLeft * speed;
+        }
+
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rb.velocity = goRight * speed;
+        }
+
+    }
+}
